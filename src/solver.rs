f1d199ba4739f03acc0718fa5d7440c3e77274ce
HardBoss07 <matteo@bosshard.net>
2025-09-13 14:02:59 +0200

    changed variable naming from idx to index

--- src/solver.rs
+++ src/solver.rs
@@ -17,36 +17,36 @@ impl Solver {
         let size = self.nonogramm.size;
 
         // rows
-        for (row_idx, clues) in self.nonogramm.rows.iter().enumerate() {
+        for (row_index, clues) in self.nonogramm.rows.iter().enumerate() {
             if self.is_golden_rule(clues) {
-                let mut idx = row_idx * size;
+                let mut index = row_index * size;
                 for (i, &clue) in clues.iter().enumerate() {
                     for _ in 0..clue {
-                        board[idx] = 'X';
-                        idx += 1;
+                        board[index] = 'X';
+                        index += 1;
                     }
 
                     if i != clues.len() - 1 {
-                        board[idx] = '.';
-                        idx += 1;
+                        board[index] = '.';
+                        index += 1;
                     }
                 }
             }
         }
 
         // cols
-        for (col_idx, clues) in self.nonogramm.cols.iter().enumerate() {
+        for (col_index, clues) in self.nonogramm.cols.iter().enumerate() {
             if self.is_golden_rule(clues) {
                 let mut row = 0;
                 for (i, &clue) in clues.iter().enumerate() {
                     for _ in 0..clue {
-                        let idx = row * size + col_idx;
-                        board[idx] = 'X';
+                        let index = row * size + col_index;
+                        board[index] = 'X';
                         row += 1;
                     }
                     if i != clues.len() - 1 {
-                        let idx = row * size + col_idx;
-                        board[idx] = '.';
+                        let index = row * size + col_index;
+                        board[index] = '.';
                         row += 1;
                     }
                 }
