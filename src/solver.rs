956e07f3e15646b4bcae733e0b75d7940a225b25
HardBoss07 <matteo@bosshard.net>
2025-09-14 16:00:23 +0200

    added coords_to_index helper function to quickly switch between 2D and 1D

--- src/solver.rs
+++ src/solver.rs
@@ -61,4 +61,30 @@ impl Solver {
     fn is_golden_rule(&self, clues: &[u8]) -> bool {
         clues.iter().map(|&x| x as usize).sum::<usize>() + (clues.len() - 1) == self.nonogramm.size
     }
+
+    /*fn is_row_solved(&self, clues: &[u8]) -> bool {
+        let mut filled_clue_len = 0;
+
+        for (i, &clue) in clues.iter().enumerate() {
+            match self.nonogramm.board.get(self.coords_to_index(i, 0)) {
+                Some(&'X') => {
+                    filled_clue_len += 1;
+                    if filled_clue_len > clue as usize {
+                        return false;
+                    } else if filled_clue_len == clue as usize {
+                        filled_clue_len = 0;
+                    }
+                }
+                Some(&'.') => return false,
+                Some(&'?') => return false,
+                _ => return false,
+            }
+        }
+
+
+    }*/
+
+    fn coords_to_index(&self, row: usize, col: usize) -> usize {
+        row * self.nonogramm.size + col
+    }
 }
