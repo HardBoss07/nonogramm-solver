a01dc494bbd567a21fbdf4706e09cd286b3717a3
HardBoss07 <matteo@bosshard.net>
2025-09-18 14:11:19 +0200

    created unified function for line checking and a general function for checking if a whole puzzle is solved

--- src/solver.rs
+++ src/solver.rs
@@ -55,6 +55,10 @@ impl Solver {
 
         // next: if only 1 clue and clue is bigger or equal to size / 2 fill in fixed places
 
+        if self.is_solved() {
+            println!("Puzzle solved");
+        }
+
         self.nonogramm.board = board;
     }
 
@@ -62,29 +66,81 @@ impl Solver {
         clues.iter().map(|&x| x as usize).sum::<usize>() + (clues.len() - 1) == self.nonogramm.size
     }
 
-    /*fn is_row_solved(&self, clues: &[u8]) -> bool {
-        let mut filled_clue_len = 0;
+    pub fn is_solved(&self) -> bool {
+        let size = self.nonogramm.size;
 
-        for (i, &clue) in clues.iter().enumerate() {
-            match self.nonogramm.board.get(self.coords_to_index(i, 0)) {
-                Some(&'X') => {
-                    filled_clue_len += 1;
-                    if filled_clue_len > clue as usize {
-                        return false;
-                    } else if filled_clue_len == clue as usize {
-                        filled_clue_len = 0;
-                    }
-                }
-                Some(&'.') => return false,
-                Some(&'?') => return false,
-                _ => return false,
+        // check all rows
+        for (row, clues) in self.nonogramm.rows.iter().enumerate() {
+            if !self.is_row_solved(row, clues) {
+                return false;
             }
         }
 
+        // check all cols
+        for (col, clues) in self.nonogramm.cols.iter().enumerate() {
+            if !self.is_col_solved(col, clues) {
+                return false;
+            }
+        }
 
-    }*/
+        true
+    }
 
     fn coords_to_index(&self, row: usize, col: usize) -> usize {
         row * self.nonogramm.size + col
+    }
+
+    fn is_row_solved(&self, row: usize, clues: &[u8]) -> bool {
+        let size = self.nonogramm.size;
+        self.is_line_solved(size, clues, |col| {
+            self.nonogramm.board.get(self.coords_to_index(row, col)).copied()
+        })
+    }
+
+    fn is_col_solved(&self, col: usize, clues: &[u8]) -> bool {
+        let size = self.nonogramm.size;
+        self.is_line_solved(size, clues, |row| {
+            self.nonogramm.board.get(self.coords_to_index(row, col)).copied()
+        })
+    }
+
+    fn is_line_solved<F>(&self, len: usize, clues: &[u8], mut get: F) -> bool
+    where
+        F: FnMut(usize) -> Option<char>,
+    {
+        let mut clue_index = 0;
+        let mut filled_clue_len = 0;
+    
+        for i in 0..len {
+            match get(i) {
+                Some('X') => {
+                    filled_clue_len += 1;
+                    if clue_index >= clues.len() || filled_clue_len > clues[clue_index] as usize {
+                        return false;
+                    }
+                }
+                Some('.') => {
+                    if filled_clue_len > 0 {
+                        if filled_clue_len != clues[clue_index] as usize {
+                            return false;
+                        }
+                        clue_index += 1;
+                        filled_clue_len = 0;
+                    }
+                }
+                Some('?') => return false,
+                _ => return false,
+            }
+        }
+    
+        // end of line: must close last group if there was one
+        if filled_clue_len > 0 {
+            if clue_index >= clues.len() || filled_clue_len != clues[clue_index] as usize {
+                return false;
+            }
+            clue_index += 1;
+        }
+    
+        clue_index == clues.len()
     }
 }
